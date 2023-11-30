6f225d8f09d5e0a12990b59ef9ef6afca0247f7e
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-11-30 18:13:00 +0100

    say-hello-to-functions

---
+++ string-manipulator.php
@@ -0,0 +1,37 @@
+<?php
+
+function capsMe($string): string
+{
+    return strtoupper($string) ;
+}
+
+function lowerMe($string): string
+{
+    return strtolower($string) ;
+}
+
+function upperCaseFirst($string): string
+{
+    return ucwords($string) ;
+}
+
+function lowerCaseFirst($string): string
+{
+    $result = upperCaseFirst($string);
+    $len = strlen($result);
+    for ($i = 0; $i < $len; $i++) {
+        if (ctype_lower($result[$i])) {
+            $result[$i] = strtoupper($result[$i]);
+        } else {
+            $result[$i] = strtolower($result[$i]);
+        }
+    }
+    return $result;
+}
+
+function removeBlankSpace($string): string
+{
+    return trim($string) ;
+}
+
+?>
