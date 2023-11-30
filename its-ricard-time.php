aff7cdf4bf51663ae465de4fa34a311625dbef3b
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-11-30 16:53:30 +0100

    its-ricard-time

--- its-ricard-time.php
+++ its-ricard-time.php
@@ -6,9 +6,9 @@ do {
     echo 'Come on ricard number ' .$nbrRicard."\n";
     if ($nbrRicard == 3) {
         echo "I'll have to stop soon!\n";
-    }elseif ($nbrRicard == 3) {
+    }elseif ($nbrRicard == 5) {
         echo "I am no longer very fresh ...\n";
     }
     $nbrRicard++;
-} while($nbrRicard < 7);
+} while($nbrRicard < 8);
 ?>
