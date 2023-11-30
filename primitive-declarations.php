6b61d067d8a6c4679deda34d81891fb109f27869
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-11-30 15:23:59 +0100

    primitive-declarations

--- primitive-declarations.php
+++ primitive-declarations.php
@@ -1,7 +1,12 @@
 <?php
 
 $firstStr = "a string of words";
-$firstEscapeStr ="";
+$firstEscapeStr = "a string of word \n";
+$firstBool = true;
+$firstInteger = 10;
+$firstFloat = 10.0;
+$firstNull = null;
+$firsrArray = array(2, null, 'Hi', 5.5, false);
 
 
 
