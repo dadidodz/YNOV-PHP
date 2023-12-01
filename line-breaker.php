e2a0b6a084874a1285093fc0e5f7532aca8bfcca
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-01 15:59:17 +0100

    line-breaker

--- line-breaker.php
+++ line-breaker.php
@@ -5,25 +5,23 @@ function breakLines($string, $int): string
     $result = '';
     $pattern = "/[-\s:]/";
     $nbr_char = 0;
-    $counter = 0;
     $array_string = preg_split($pattern, $string);
     foreach ($array_string as $value){
         $len_word=strlen($value);
         if ($len_word > $int){
             throw new Exception('A word is longer than '.$int.' characters');
         }else{
-            $nbr_char+=$len_word;
-            if($counter == 0 and $nbr_char < $int){
+            if($nbr_char==0 and $nbr_char < $int){
                 $result.=$value;
+                $nbr_char+=$len_word;
             }elseif ($nbr_char < $int){
                 $result.= ' '.$value;
-                $nbr_char+=1;
+                $nbr_char+=$len_word+1;
             }else{
                 $result.= "\n".$value;
                 $nbr_char=0;
             }
         }
-        $counter+=1;
     }
     return $result;
 }
