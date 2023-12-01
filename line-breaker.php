d2997bded8098308d4f2690367abaf2820f8d671
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-01 16:34:17 +0100

    line-breaker

--- line-breaker.php
+++ line-breaker.php
@@ -1,32 +1,27 @@
 <?php
 
-function breakLines($string, $int): string
+function breakLines($string, $maxLength): string
 {
+    $lines = explode("\n", $string);
     $result = '';
-    $pattern = "/[-\s:]/";
-    $nbr_char = 0;
-    $retour_ligne = true;
-    $array_string = preg_split($pattern, $string);
-    foreach ($array_string as $value){
-        $len_word=strlen($value);
-        if ($len_word > $int){
-            throw new Exception('A word is longer than '.$int.' characters');
-        }else{
-            $nbr_char += $len_word;
-            if($retour_ligne == true and $nbr_char < $int){
-                $result.=$value;
-                $retour_ligne = false;
-            }elseif ($nbr_char < $int){
-                $result.= ' '.$value;
-                $nbr_char+=$len_word+1;
-            }else{
-                $result.= "\n".$value.' ';
-                $nbr_char=$len_word+1;
-                $retour_ligne = true;
+
+    foreach ($lines as $line) {
+        $words = explode(' ', $line);
+        $currentLine = '';
+
+        foreach ($words as $word) {
+            if (strlen($currentLine . $word) <= $maxLength) {
+                $currentLine .= $word . ' ';
+            } else {
+                $result .= rtrim($currentLine) . "\n";
+                $currentLine = $word . ' ';
             }
         }
+        $result .= rtrim($currentLine) . "\n";
     }
+    $result = rtrim($result);
+
     return $result;
 }
-echo breakLines('Title is long\nLine with words break', 12)
+
 ?>
