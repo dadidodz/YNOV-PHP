36b1e1f96891e06214941627d9cd9fd8d42580fc
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-01 14:28:49 +0100

    pangram

--- pangram.php
+++ pangram.php
@@ -5,11 +5,13 @@ function isPangram($string): bool
     $string = strtoupper($string);
     $alphas = range('A', 'Z');
     foreach ($alphas as $key => $value){
-        if (preg_match($value, $string)==0){
+        $pattern = "/".$value."/i";
+        if (preg_match($pattern, $string)==0){
             return false;
             break;
         }
     }
     return true;
 }
+
 ?>
