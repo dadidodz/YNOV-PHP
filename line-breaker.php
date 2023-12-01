115ddcfe4520bea1b0205a195aaf9698347dbaae
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-01 15:38:01 +0100

    line-breaker

--- line-breaker.php
+++ line-breaker.php
@@ -5,6 +5,7 @@ function breakLines($string, $int): string
     $result = '';
     $pattern = "/[-\s:]/";
     $nbr_char = 0;
+    $counter = 0;
     $array_string = preg_split($pattern, $string);
     foreach ($array_string as $value){
         $len_word=strlen($value);
@@ -12,13 +13,16 @@ function breakLines($string, $int): string
             throw new Exception('A word is longer than '.$int.' characters');
         }else{
             $nbr_char+=$len_word;
-            if ($nbr_char < $int){
+            if($counter == count($array_string) - 1 and $nbr_char < $int){
+                $result.=$value;
+            }elseif ($nbr_char < $int){
                 $result.=$value.' ';
             }else{
                 $result.= "\n".$value;
                 $nbr_char=0;
             }
-        };
+        }
+        $counter+=1;
     }
     return $result;
 }
