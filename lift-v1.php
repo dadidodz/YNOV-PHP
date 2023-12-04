6d921a1a47dce0334921f15a4524af1704a179b0
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-04 16:18:35 +0100

    lift-v1

---
+++ lift-v1.php
@@ -0,0 +1,57 @@
+<?php
+
+function getFloor(int $currentFloor, int|null $requestedFloor, array $listButtonsCalled){
+
+    if ($requestedFloor != null){
+        return $requestedFloor;
+    }
+
+    if (!empty($listButtonsCalled)){
+        $nearestFloor = getNearestNumberInArray($currentFloor, $listButtonsCalled);
+        return $nearestFloor;
+    }
+
+    return null;
+
+}
+
+function getNearestNumberInArray($numbers, $reference) {
+    if (empty($reference)) {
+        return null;
+    }
+
+    $nearest = null;
+    $nearestDistance = null;
+
+    foreach ($reference as $number) {
+        $distance = abs($number - $numbers);
+
+        if ($nearestDistance === null || $distance < $nearestDistance) {
+            $nearest = $number;
+            $nearestDistance = $distance;
+        }
+    }
+
+    return $nearest;
+}
+
+function getDirection(int $currentFloor, int|null $requestedFloor, array $listButtonsCalled): int{
+
+    if ($currentFloor === $requestedFloor) {
+        return 0;
+    }
+
+    $nearestFloor = getNearestNumberInArray($currentFloor, $listButtonsCalled);
+
+    if ($currentFloor < $requestedFloor || $currentFloor < $nearestFloor) { 
+        return 1;
+    }
+    else {
+        return -1;
+    }
+    if (empty($calledFloors)) {
+        return 0;
+    }
+}
+
+?>
