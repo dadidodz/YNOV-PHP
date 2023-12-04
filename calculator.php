f49a5da9dbcf93b321489f1fce9b299c91457f7e
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-04 15:26:38 +0100

    calculator

---
+++ calculator.php
@@ -0,0 +1,14 @@
+<?php
+
+function calc(string $expression): int
+{
+    if (preg_match('/[^0-9+\-() ]/', $expression)){
+        throw new InvalidArgumentException('Invalid character');
+    }
+
+    $expression = str_replace(' ', '', $expression);
+    $result = eval("return $expression;");
+    return $result;
+}
+
+?>
