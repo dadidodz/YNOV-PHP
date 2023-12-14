900622c510ddd92161d75ca1f507a0f2a36d1a15
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-14 16:58:04 +0100

    thats-anonymous

--- thats-anonymous.php
+++ thats-anonymous.php
@@ -1,27 +1,18 @@
 <?php
 
-$dayNumber = date("jS");
-$month = date("F");
-$year = date("Y");
 
 
-$today = function() use ($dayNumber, $month, $year){
+
+$today = function() {
+    $dayNumber = date("jS");
+    $month = date("F");
+    $year = date("Y");
     $result = 'It is ' . $month . ' ' . $dayNumber . ', ' . $year;
     echo $result;
 };
 
-$isLeapYear = function() use ($year){
-    if ($year % 400 == 0)
-        return True;
-    else if ($year % 100 == 0)
-        return False;
-    else if ($year % 4 == 0)
-        return True;
-    else
-        return False;
-};
 
 $today();
-$isLeapYear();
+
 
 ?>
