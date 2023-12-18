87044a4f21ec9d9f3ddc5c421f5ea3c3812d99d9
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-18 15:10:05 +0100

    join

--- join.php
+++ join.php
@@ -1,14 +1,18 @@
 <?php
 
-function joinWords(array $tab, string $separator = ' '){
+function joinWords(array $tab, string $separator = ' '): string {
 
+    $counter = 0;
     $result = '';
     foreach($tab as $word){
-        $result = $result . $separator . $word;
+        if ($counter < 1){
+            $result .= $word;
+            $counter += 1;
+        }else{
+            $result .= $separator . $word;
+        }
     }
     return $result;
 
 }
-
-
 ?>
