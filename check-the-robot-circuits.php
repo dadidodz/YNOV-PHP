be3e8b51e6f750ab041e1c9768ef95adb7b4bb08
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-18 16:53:36 +0100

    check-the-robot-circuits

---
+++ check-the-robot-circuits.php
@@ -0,0 +1,23 @@
+<?php 
+
+function checkCircuits(int $number): array{
+    $finalArr = [];
+    $tab = [
+        2 => 'Left arm',
+        3 => 'Right arm',
+        5 => 'Motherboard',
+        7 => 'Processor',
+        11 => 'Zip Defluxer',
+        13 => 'Motor'
+    ];
+
+    foreach($tab as $digit) {
+        if (($number % $digit) == 0){
+            array_push($finalArr, $tab[$digit]);
+        }
+    }
+
+    return $finalArr;
+}
+
+?>
