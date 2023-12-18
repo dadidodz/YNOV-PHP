c82d2a90013cf1a92bd78b97626f414c3b599d73
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-18 16:31:24 +0100

    manage-robot-movement

--- manage-robot-movements.php
+++ manage-robot-movements.php
@@ -2,18 +2,18 @@
 function manageMovements(string $commands): array{
     $finalArr = [];
     $tab = [
-        "R" => "RIGHT",
-        "L" => "LEFT",
-        "F" => "FORWARD",
-        "B" => "BACKWARD"
+        'R' => 'RIGHT',
+        'L' => 'LEFT',
+        'F' => 'FORWARD',
+        'B' => 'BACKWARD'
     ];
-    $word = "";
-    $last = "";
+    $word = '';
+    $last = '';
 
 
     for ($i = 0; $i < strlen($commands); $i++){
         if ($commands[$i] == $last) {
-            $word = $tab[$commands[$i]] . " AGAIN";
+            $word = $tab[$commands[$i]] . ' AGAIN';
             array_push($finalArr, $word);
         }else{
             array_push($finalArr, $tab[$commands[$i]]);
