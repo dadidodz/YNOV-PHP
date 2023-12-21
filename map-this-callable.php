3cc29e22a4c56bcc13e39c80dfc07175641bddfc
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-21 15:57:27 +0100

    map-this-callable

--- map-this-callable.php
+++ map-this-callable.php
@@ -1,6 +1,9 @@
 <?php
 
 function myArrayMap(?callable $callable, array $array, array ...$arrays): array{
+    if ($callable == null){
+        return $array;
+    }
     for ($i = 0; $i < count($array); $i++) {
         $array[$i] = $callable($array[$i]); //sum
     }
