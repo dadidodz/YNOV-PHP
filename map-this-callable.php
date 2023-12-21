e9a6575b40bdd3495a42516d142fb728bf83bec7
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-21 16:31:58 +0100

    map-this-callable

--- map-this-callable.php
+++ map-this-callable.php
@@ -10,32 +10,4 @@ function myArrayMap1(?callable $callable, array $array, array ...$arrays): array
     return $array;
 }
 
-
-function myArrayMap(?callable $callback, array $array, array ...$arrays) {
-     // Check if all arrays have the same length
-     $lengths = array_map('count', $arrays);
-     if (count(array_unique($lengths)) !== 1) {
-         throw new InvalidArgumentException("Input arrays must have the same length");
-     }
- 
-     $result = [];
-     $numArrays = count($arrays);
-     $length = $lengths[0];
- 
-     // Iterate through each element of the arrays
-     for ($i = 0; $i < $length; $i++) {
-         $args = [];
- 
-         // Extract the i-th element from each array
-         for ($j = 0; $j < $numArrays; $j++) {
-             $args[] = $arrays[$j][$i];
-         }
- 
-         // Apply the callback to the elements and store the result
-         $result[] = call_user_func_array($callback, $args);
-     }
- 
-     return $result;
-}
-
 ?>
