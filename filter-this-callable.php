0461e6617f8c57283ac27067b386738f6f03b6ca
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-21 17:48:05 +0100

    filter-this-callable

--- filter-this-callable.php
+++ filter-this-callable.php
@@ -2,18 +2,19 @@
 function myArrayFilter(array $array, ?callable $callback = null): array{
     $result = [];
     if ($callback == null){
-        for ($i = 0; $i < count($array); $i++) {
-            if (!empty($array[$i])){
-                array_push($result, $array[$i]);
+        foreach ($array as $key => $value) {
+            if (!empty($array[$key])){
+                $result[$key] = $value;
             }
         }
     }else{
-        for ($i = 0; $i < count($array); $i++) {
-            if ($callback($array[$i]) == 1){
-                $result[$i] = $array[$i];
+        foreach ($array as $key => $value) {
+            if ($callback($array[$key]) == 1){
+                $result[$key] = $value;
             }
         }
     }
     return $result;
 }
+
 ?>
