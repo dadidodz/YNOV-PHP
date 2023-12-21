684d33194bbf9bd3161b3749bf7c6838a47735da
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2023-12-21 16:05:33 +0100

    map-this-callable

--- map-this-callable.php
+++ map-this-callable.php
@@ -12,25 +12,30 @@ function myArrayMap1(?callable $callable, array $array, array ...$arrays): array
 
 
 function myArrayMap(?callable $callback, array $array, array ...$arrays) {
-    // Check if all arrays have the same length
-    $lengths = array_map('count', $arrays);
-    if (count(array_unique($lengths)) !== 1) {
-        throw new InvalidArgumentException("Input arrays must have the same length");
-    }
-
-    $result = [];
-
-    // Iterate through each element of the arrays
-    for ($i = 0; $i < $lengths[0]; $i++) {
-        $args = array_map(function ($array) use ($i) {
-            return $array[$i];
-        }, $arrays);
-
-        // Apply the callback to the elements and store the result
-        $result[] = call_user_func_array($callback, $args);
-    }
-
-    return $result;
+     // Check if all arrays have the same length
+     $lengths = array_map('count', $arrays);
+     if (count(array_unique($lengths)) !== 1) {
+         throw new InvalidArgumentException("Input arrays must have the same length");
+     }
+ 
+     $result = [];
+     $numArrays = count($arrays);
+     $length = $lengths[0];
+ 
+     // Iterate through each element of the arrays
+     for ($i = 0; $i < $length; $i++) {
+         $args = [];
+ 
+         // Extract the i-th element from each array
+         for ($j = 0; $j < $numArrays; $j++) {
+             $args[] = $arrays[$j][$i];
+         }
+ 
+         // Apply the callback to the elements and store the result
+         $result[] = call_user_func_array($callback, $args);
+     }
+ 
+     return $result;
 }
 
 ?>
