d3145df13f54eff6048397bbf9b8641f1fc42051
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2024-01-11 17:56:26 +0100

    geometry-master

---
+++ geometry-master.php
@@ -0,0 +1,45 @@
+<?php
+
+abstract class AbstractGeometry {
+    
+    abstract public function area();
+    abstract public function perimeter();
+}
+
+class Rectangle extends AbstractGeometry {
+
+    public function area(){
+
+    }
+
+    public function perimeter(){
+
+    }
+}
+
+
+class Triangle extends AbstractGeometry {
+    
+    public function area(){
+
+    }
+
+    public function perimeter(){
+        
+    }
+}
+
+
+class Square extends AbstractGeometry {
+    
+    public function area(){
+
+    }
+
+    public function perimeter(){
+        
+    }
+}
+
+
+?>
