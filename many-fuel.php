abaf427c0a8347747c1687df5f60494fb3c5c012
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2024-01-11 16:57:24 +0100

    many-fuel

--- many-fuel.php
+++ many-fuel.php
@@ -1,35 +1,35 @@
 <?php
 
-class Car{
-    //  ↙ Visibility on attributes
-    private float $tank;
+    class Car{
+        //  ↙ Visibility on attributes
+        private float $tank;
 
-    //  ↙ Visibility into functions
-    public function __construct(float $tank = 45.4)
-    { 
-        $this->tank = $tank;
+        //  ↙ Visibility into functions
+        public function __construct(float $tank = 0)
+        { 
+            $this->tank = $tank;
+
+        }
+
+        // Getters
+        public function getTank(): float
+        {
+            return $this->tank;
+        }
+
+        // Setters
+        public function setTank(float $tank): self
+        {
+            $this->tank = $tank;
+            return $this;
+        }
+
+        public function ride(float $distance){
+            (float)$consommation = $distance / 20;
+            $this->setTank($this->getTank()-$consommation);
+        }
+        
 
     }
-
-    // Getters
-    public function getTank(): float
-    {
-        return $this->tank;
-    }
-
-    // Setters
-    public function setTank(float $tank): self
-    {
-        $this->tank = $tank;
-        return $this;
-    }
-
-    public function ride(float $distance){
-        (float)$consommation = $distance / 20;
-        $this->setTank($this->getTank()-$consommation);
-    }
-    
-
- }
 
 ?>
