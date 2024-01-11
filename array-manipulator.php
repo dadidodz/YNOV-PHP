fc35ca4da782c7e317a91a81af11d19ae718ec12
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2024-01-11 15:52:44 +0100

    array-manipulator

---
+++ array-manipulator.php
@@ -0,0 +1,45 @@
+<?php
+
+function reverse(array $array): array {
+    $reversedArray = [];
+    for ($i = count($array)-1; $i >= 0; $i--){
+        $reversedArray[] = $array[$i];
+    }
+    return $reversedArray;
+}
+
+function push(array &$array, string ...$strings): int {
+    foreach($strings as $string){
+        $array[] = $string;
+    }
+    return count($array);
+}
+
+function sum(array $array): int {
+    $result = 0;
+    foreach($array as $value){
+        $result += $value;
+    }
+    return $result;
+}
+
+function arrayContains(array $array, int|string|float $element): mixed{
+    foreach($array as $value){
+        if ($value == $element){
+            return $value;
+        }
+    }
+    return null;
+}
+
+function merge(array ...$arrays){
+    $result = [];
+    foreach($arrays as $array) {
+        foreach($array as $value){
+            array_push($result, $value);
+        }
+    }
+    return $result;
+}
+
+?>
