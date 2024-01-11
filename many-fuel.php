5099762e03cfac2a1065fd1d70d6d5209d701f0a
Dorian Blatiere <dorian.blatiere@hotmail.fr>
2024-01-11 16:42:31 +0100

    i-want-a-cocktail

---
+++ many-fuel.php
@@ -0,0 +1,33 @@
+<?php
+
+class Car{
+    //  ↙ Visibility on attributes
+    private float $tank;
+
+    //  ↙ Visibility into functions
+    public function __construct(float $tank)
+    {
+        $this->tank = $tank;
+    }
+
+    // Getters
+    public function getTank(): float
+    {
+        return $this->tank;
+    }
+
+    // Setters
+    public function setTank(float $tank): self
+    {
+        $this->tank = $tank;
+        return $this;
+    }
+
+    public function ride(float $distance){
+        $consommation = $distance / 20;
+        $this->setTank($this->getTank()-$consommation);
+    }
+
+ }
+
+?>
